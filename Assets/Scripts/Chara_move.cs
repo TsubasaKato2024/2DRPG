6b1043c6a230b5582f619f1d22f4035b87d62508
TsubasaKato2024 <uni202406_06@telework.to>
2024-11-13 14:01:07 +0900

    フルスクリーンバグ修正

--- Assets/Scripts/Chara_move.cs
+++ Assets/Scripts/Chara_move.cs
@@ -1,40 +1,50 @@
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class Chara_move : MonoBehaviour
 {
-    private float speed = 0.01f; //floatは小数点
+    public float speed = 3f; //floatは小数点
     private Animator animator;
+    private Rigidbody2D rigidbody2D;
+    Vector2 pos = Vector2.zero;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Vector2 pos = transform.position;
+        pos = Vector2.zero;
+
         if (Input.GetKey(KeyCode.S))//↓キーを押したら
         {
-            pos.y -= speed;
+            pos.y = -speed;
             animator.SetInteger("direction", 0);//正面を向く
         }
         else if (Input.GetKey(KeyCode.A))//←キーを押したら
         {
 
-            pos.x -= speed;
+            pos.x = -speed;
             animator.SetInteger("direction", 1);//左を向く
         }
         else if (Input.GetKey(KeyCode.D))//→キーを押したら
         {
-            pos.x += speed;
+            pos.x = speed;
             animator.SetInteger("direction", 2);//右を向く
 
         }
         else if (Input.GetKey(KeyCode.W))//↑キーを押したら
         {
-            pos.y += speed;
+            pos.y = speed;
             animator.SetInteger("direction", 3);//後ろを向く
         }
-        transform.position = pos;
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = pos;
     }
 }
