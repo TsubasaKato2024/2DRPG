3d4801558c620771d0780f6693331a2dbd57d186
TsubasaKato2024 <uni202406_06@telework.to>
2024-11-19 12:04:25 +0900

    アニメーションの修正、シーン編集など

---
+++ Assets/Scripts/Scene/Scene_Clear.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scene_Clear : MonoBehaviour
+{
+    public GameObject dialogue;
+    public Text Text;
+
+    [SerializeField]
+    string words = "";
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Text.text = words;
+            dialogue.SetActive(true);
+            StartCoroutine(WaitAndChangeScene(2f)); // 5秒後にシーン遷移
+        }
+    }
+
+    // コルーチンで指定された時間（秒数）後にシーン遷移
+    private IEnumerator WaitAndChangeScene(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime); // 2秒待つ
+        SceneManager.LoadScene("ClearScene"); // シーン遷移
+    }
+}
